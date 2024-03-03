92e6221f458deca5cc4317f2b28c14c24ee29c0f
Amanda Zambrana <126283318+amanda-zambrana@users.noreply.github.com>
2024-03-03 15:43:57 -0500

    Update testGithubAPI.py file with mock tests

--- testGithubAPI.py
+++ testGithubAPI.py
@@ -6,29 +6,38 @@ githubAPI.py file.
 that there are no repositories in that account when we run the tests to ensure accurate test results:
 https://github.com/AmandaZamora)
 
+---
+
+This code has also been updated & designed to use a python mocking package to mock this program's external dependence
+on GitHub so that I can guarantee that the unit tests will run consistently every time that they run, no 
+matter how many times I run them, and no matter what changes I make to the repository. 
+
+This program, mockGithubAPI.py starts with the identical code from the githubAPI.py program, and mocks out 
+all of the service calls in that program using the python mock module from unittest, unittest.mock . 
+
 @author: amanda-zambrana
 """
 
 import unittest
+from unittest.mock import patch
 from githubAPI import githubapi
 
 class testGithubAPI(unittest.TestCase):
     # This test case is for testing when there is no user account with the inputted name 
-    def testNoAccount(self):
+    @patch('githubAPI.githubapi', return_value=False)
+    def testNoAccount(self, mock_githubapi):
         self.assertEqual(githubapi('amanda-zambrana123456789'), False)
 
     # This test case is for when a username inputted does correlate with an account in GitHub that does exist 
-    def testExistingAccount(self): 
+    @patch('githubAPI.githubapi', return_value=True)
+    def testExistingAccount(self, mock_githubapi): 
         self.assertEqual(githubapi('amanda-zambrana'), True)
     
     # This test case is for when the user input does correspond with an existing account, but there is no public repos.
-    def testNoRepos(self):
+    @patch('githubAPI.githubapi', return_value=False)
+    def testNoRepos(self, mock_githubapi):
         self.assertEqual(githubapi('AmandaZamora'), False) # Please note that I found a random GitHub account that exists with no repos just for this test
-
-   # def testNoInput(self):
-   #     self.assertEqual(githubapi(''), False,'There is no input')
 
 if __name__ == '__main__':
     print("Running unit tests.")
     unittest.main()
-
